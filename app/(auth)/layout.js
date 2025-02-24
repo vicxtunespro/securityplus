92ad8c62523d08ad7bd286d624c5a267dffb210d
Nuwarimpa Victor <124519343+vicxtunespro@users.noreply.github.com>
2025-02-24 15:05:39 +0000

    Minize servles function size

--- app/(auth)/layout.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function AuthLayout({ children }) {
-  return (
-    <div className="flex text-white items-center justify-center h-full bg-sky-500">
-        {children}
-    </div>
-  )
-}
--- app/(auth)/login/page.js
+++
@@ -1,8 +0,0 @@
-import { LoginForm } from '@/components/auth/login-form'
-import React from 'react'
-
-export default function LoginPage() {
-  return (
-    <LoginForm />
-  )
-}
